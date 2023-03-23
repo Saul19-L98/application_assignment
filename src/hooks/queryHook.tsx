82cbaf20bab66f6953bcf0088698a028a16bebd9
sallanez <saul.alejandro19@gmail.com>
2023-03-22 18:16:05 -0600

    user with employee role in Home.tsx is working

--- src/hooks/queryHook.tsx
+++ src/hooks/queryHook.tsx
@@ -2,7 +2,7 @@ import {ApplicationData,EmployeesData} from "../interfaces/sessionTypes";
 import { useQuery } from 'react-query';
 import { db } from '../firebase.config';
 import { useUserCredentialsStore } from '../store/userCredentialsStore';
-import { collection, getDocs, DocumentData } from 'firebase/firestore';
+import { collection, getDocs, DocumentData,getDoc,doc,where,query } from 'firebase/firestore';
 
 const fetchApplications = async (): Promise<DocumentData[]> => {
     const applicationsRef = collection(db, 'applications');
@@ -23,6 +23,29 @@ const fetchEmployees = async (): Promise<DocumentData[]> => {
     return employeesData;
 };
 
+const fetchApplicationsByEmployeeId = async (employeeId: string): Promise<DocumentData[]> => {
+    const applicationsRef = collection(db, 'applications');
+    const q = query(applicationsRef, where("employeeId", "==", employeeId));
+    const applicationsDocs = await getDocs(q);
+    const applications: DocumentData[] = applicationsDocs.docs.map((doc) => ({
+        ...doc.data(),
+        applicationId: doc.id,
+    }));
+    return applications;
+}
+
+const fetchEmployee = async (employeeId: string): Promise<DocumentData> => {
+    const employeeRef = doc(db, 'employees', employeeId);
+    const employeeDoc = await getDoc(employeeRef);
+    const employeeData = {
+        ...employeeDoc.data(),
+        employeeId: employeeDoc.id,
+    };
+    return employeeData;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+//NOTE: Work with Dashborad.tsx
 export function useApplications() {
     const {setApplications} = useUserCredentialsStore();
     return useQuery<DocumentData[], Error>('applications', fetchApplications, {
@@ -40,3 +63,26 @@ export function useEmployees(){
         },
     });
 }
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+//NOTE: Work with Home.tsx
+export function useApplicationByEmployee(employeeId: string) {
+    const { setApplications } = useUserCredentialsStore();
+    return useQuery<DocumentData[], Error>(['applications', employeeId], () => fetchApplicationsByEmployeeId(employeeId), {
+        onSuccess: (data) => {
+            setApplications(data as ApplicationData[]);
+        },
+    });
+};
+
+export function useEmployee(employeeId: string) {
+    const { setEmployees } = useUserCredentialsStore();
+    return useQuery<DocumentData, Error>(['employee', employeeId], () => fetchEmployee(employeeId), {
+        onSuccess: (data) => {
+            setEmployees([data] as EmployeesData[]);
+        },
+    });
+}
+/////////////////////////////////////////////////////////////////////////////
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,73 @@
 import { useContextHook} from "../hooks/authContext";
+import { useApplicationByEmployee,useEmployee } from '../hooks/queryHook';
 import { useUserCredentialsStore } from "../store/userCredentialsStore";
+import {getEmployeeName} from "../helpers/getUserName"
+import ModalForm from "../components/ModalForm";
+import Spinner from "../components/Spinner";
+import Table from "../components/Table";
 
 function Home(){
-    const {userCredentials,setUserCredentials} = useUserCredentialsStore();
+    const {setUserCredentials,setEmployees,setApplications,userData,employees} = useUserCredentialsStore();
     const {logOut} = useContextHook();
-    console.log(`What is this Home: ${userCredentials?.user}`);
-    console.log(userCredentials?.user)
+
+    const { isLoading: isApplicationsLoading, isError: isApplicationsError, refetch: refetchApplications } = useApplicationByEmployee(userData!.employeeId);
+
+    const { data: employeeData, isLoading: isEmployeeLoading, isError: isEmployeeError, error: employeeError } = useEmployee(userData!.employeeId);
+
+    //Get first letter of the employee name
+    const getFirstLetterOfEmployeeName = () => {
+        const employeeName = getEmployeeName(userData!.employeeId,employees!);
+        return employeeName.charAt(0).toUpperCase();
+    };
+
     const handleLogOut = async () => {
         await logOut();
         setUserCredentials(null);
+        setEmployees(null);
+        setApplications(null);
+    }
+
+
+    if (isApplicationsLoading && isEmployeeLoading) {
+        return <Spinner />;
+    }
+    
+    if (isApplicationsError && isEmployeeError) {
+    return <Spinner />;
     }
 
     return(
         <div>
-            <h1>Hello from DashBoard (Employee)</h1>
-            <h1>{userCredentials?.user!.email}</h1>
+            <div className="flex flex-col sm:flex-row justify-between items-center">
+                <div className="card w-full bg-base-100 shadow-xl m-4 md:w-72">
+                    <div className="flex justify-center p-4">
+                        <div className="avatar online placeholder">
+                            <div className="bg-neutral-focus text-neutral-content rounded-full w-20">
+                                <span className="text-xl">{getFirstLetterOfEmployeeName()}</span>
+                            </div>
+                        </div>
+                        <div className="flex flex-col ml-2">
+                            <div>
+                                <h1>{getEmployeeName(userData!.employeeId,employees!)}</h1>
+                            </div>
+                            <div>
+                                <button className="btn btn-primary mt-2 sm:mt-0" onClick={handleLogOut}>
+                                    Sign Out
+                                </button>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+                <div className="mb-4 sm:mb-0 sm:mr-4">
+                    <label htmlFor="my-modal" className="btn btn-primary">Create Application</label>
+                    <input type="checkbox" id="my-modal" className="modal-toggle" />
+                    <div className="modal">
+                        <ModalForm refetchApplications={refetchApplications} />
+                    </div>
+                </div>
+            </div>
             <div>
-                <button className="btn btn-primary" onClick={handleLogOut}>
-                    Sign Out
-                </button>
+                <Table />
             </div>
         </div>
     )
