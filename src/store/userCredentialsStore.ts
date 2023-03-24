e91d919ee7e26d363cc5d23e3329acab14c26930
sallanez <saul.alejandro19@gmail.com>
2023-03-24 16:40:38 -0600

    persist data added

--- src/store/userCredentialsStore.ts
+++ src/store/userCredentialsStore.ts
@@ -1,8 +1,9 @@
-import {create} from 'zustand';
-import {UserCredential} from "firebase/auth";
-import {UserData,EmployeesData,ApplicationData} from "../interfaces/sessionTypes"
+    import { create } from 'zustand';
+    import { persist,createJSONStorage } from 'zustand/middleware';
+    import { UserCredential } from 'firebase/auth';
+    import { UserData, EmployeesData, ApplicationData } from '../interfaces/sessionTypes';
 
-interface UserCredentialsState {
+    interface UserCredentialsState {
     userCredentials: UserCredential | null;
     employees: EmployeesData[] | null;
     applications: ApplicationData[] | null;
@@ -12,21 +13,29 @@ interface UserCredentialsState {
     setApplications: (applications: ApplicationData[] | null) => void;
     removeApplication: (applicationId: string) => void;
     setUserData: (user: UserData | null) => void;
-}
+    }
 
-export const useUserCredentialsStore = create<UserCredentialsState>((set) => ({
-    userCredentials: null,
-    employees: null,
-    applications: null,
-    userData:null,
-    setUserCredentials: (credentials) => set({ userCredentials: credentials }),
-    setEmployees: (employees) => set({ employees: employees }),
-    setApplications: (applications) => set({ applications: applications }),
-    removeApplication: (applicationId) =>
-    set((state) => ({
-        applications: state.applications!.filter(
-            (application) => application.applicationId !== applicationId
-        ),
-    })),
-    setUserData: (user) => set({ userData: user }),
-}));
+    export const useUserCredentialsStore = create(
+    persist<UserCredentialsState>(
+        (set) => ({
+        userCredentials: null,
+        employees: null,
+        applications: null,
+        userData: null,
+        setUserCredentials: (credentials) => set({ userCredentials: credentials }),
+        setEmployees: (employees) => set({ employees: employees }),
+        setApplications: (applications) => set({ applications: applications }),
+        removeApplication: (applicationId) =>
+            set((state) => ({
+            applications: state.applications!.filter(
+                (application) => application.applicationId !== applicationId
+            ),
+            })),
+        setUserData: (user) => set({ userData: user }),
+        }),
+        {
+            name: 'userCredentialsStorage', // unique name for the storage
+            storage: createJSONStorage(() => localStorage), // using localStorage
+        }
+    )
+    );
